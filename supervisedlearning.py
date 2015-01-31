c2fb8fcecef774c579ea8239d77c3f22253a3fd2
Rohit Pathak <rohitpathak.3@gmail.com>
2015-01-30 20:01:01 -0500

    Boosting, SVM

--- supervisedlearning.py
+++ supervisedlearning.py
@@ -1,8 +1,11 @@
 from pprint import pprint
 from sklearn import tree
+from sklearn import svm
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import classification_report
 
 
+######## GENERATE INSTANCES ########
 f = open('heart-data/SPECT.train', 'r')
 set_one_train = [ [int(n) for n in line.rstrip().split(',')] for line in f.readlines() ]
 f.close()
@@ -17,21 +20,43 @@ print "one train_x", one_train_x
 one_test_x = [ a[1:] for a in set_one_test ]
 one_test_y = [ a[0] for a in set_one_test ]
 
-print "one_train_y", one_train_y
+def generate_report(classifier, predicted, real):
+	pass
 
 
-# for i in range(len(one_train_x)):
-# 	print data_set_one[i]
-# 	print one_train_y[i], one_train_x[i]
-
+######## DECISION TREES ########
 print "training decision tree"
 dt = tree.DecisionTreeClassifier()
 dt.fit(one_train_x, one_train_y)
 
 one_pred_y = [ dt.predict(x)[0] for x in one_test_x ]
-print one_pred_y
 
 print dt.score( one_test_x, one_test_y )
 print classification_report(one_test_y, one_pred_y, target_names=['class0', 'class1'])
+
+######## NEURAL NETS ########
+
+######## BOOSTING ########
+bdt_discrete = AdaBoostClassifier(
+    tree.DecisionTreeClassifier(),
+    n_estimators=600,
+    learning_rate=1.5,
+    algorithm="SAMME")
+bdt_discrete.fit(one_train_x, one_train_y)
+one_pred_y = [ bdt_discrete.predict(x)[0] for x in one_test_x ]
+print bdt_discrete.score( one_test_x, one_test_y )
+print classification_report(one_test_y, one_pred_y, target_names=['class0', 'class1'])
+
+
+######## SUPPORT VECTOR MACHINES ########
+svc = svm.SVC()
+svc.fit(one_train_x, one_train_y)
+one_pred_y = [ svc.predict(x)[0] for x in one_test_x ]
+print svc.score( one_test_x, one_test_y )
+print classification_report(one_test_y, one_pred_y, target_names=['class0', 'class1'])
+
+
+######## kNN ########
+
 
 
