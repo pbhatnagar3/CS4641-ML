e3c53a9a97e022fd094d8d2246d9a26361ef30cb
Rohit Pathak <rohitpathak.3@gmail.com>
2015-02-08 21:32:14 -0500

    Neural nets, boosting, and svm

--- supervisedlearning.py
+++ supervisedlearning.py
@@ -1,16 +1,15 @@
 from sklearn import tree
-from sklearn import svm
+from sklearn import svm, cross_validation
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix, f1_score, precision_score
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.utilities import percentError
 from random import shuffle
 import numpy as np
 import matplotlib.pyplot as plt
-from pybrain.utilities import percentError
-from sklearn.neighbors import KNeighborsClassifier
-from pprint import pprint
 import json
 import sys
 
@@ -55,15 +54,15 @@ sphere.test.y = [ a[-1] for a in data['test'] ]
 # sphere.raw = 
 
 
-def generate_report(predicted, real, target_names=['class0', 'class1']):
-	print classification_report(real, predicted, target_names=target_names)
+def generate_report(predicted, real):
+	print classification_report(real, predicted)
 	print "Confusion Matrix:"
 	print confusion_matrix(real, predicted)
 	print '.....................'
 
 
 def test_decision_trees(ds):
-	depths = [None, 2, 4, 6, 8]
+	depths = [2, 4, 6, 8, None]
 	for depth in depths:
 		print "Depth:", depth
 		dt = tree.DecisionTreeClassifier(max_depth=depth)
@@ -72,13 +71,124 @@ def test_decision_trees(ds):
 		print "Score:", dt.score( ds.test.x, ds.test.y)
 		generate_report(pred_y, ds.test.y)
 
-print "DECISION TREES\n"
-print "Heart SPECT"
-test_decision_trees(heart)
+# print "DECISION TREES\n"
+# print "Heart SPECT"
+# test_decision_trees(heart)
+# print "Ionosphere"
+# test_decision_trees(sphere)
 
-# print "Ionosphere\n"
-# dt = tree.DecisionTreeClassifier()
-# dt.fit(two_train_x, two_train_y)
-# two_pred_y = dt.predict( two_test_x )
-# print "Score:", dt.score( two_test_x, two_test_y )
-# generate_report( dt, two_pred_y, two_test_y )
+def test_neural_nets(ds):
+
+	def plot_errors(x, train_err, test_err):
+		plt.plot(x, train_err, label='Training accuracy')
+		plt.plot(x, test_err, marker='o', linestyle='..', color='r', label='Test Accuracy')
+		plt.xlabel('Epochs')
+		plt.ylabel('Accuracy')
+		plt.title('Training and test accuracies')
+		plt.legend()
+		plt.show()
+
+	input_size = len(ds.train.x[0]) # no. of attributes
+	target_size = 1
+	hidden_size = 10
+	iterations = 1000
+	train_nnds = SupervisedDataSet(input_size, target_size)
+	train_nnds.setField('input', ds.train.x)
+	one_train_reshaped = np.array(ds.train.y).reshape(-1,1) 
+	train_nnds.setField('target', one_train_reshaped)
+	net = buildNetwork( input_size, hidden_size, target_size, bias = True )
+	trainer = BackpropTrainer( net, train_nnds )
+	epochs, train_acc, test_acc = [], [], []
+	
+	for i in xrange(iterations):
+		trainer.train()
+		train_pred_y = []
+		# Compute percent training error
+		for row in ds.train.x:
+			p = int( round( net.activate(row)[0] ) )
+			if p >= 1: p = 1 
+			else: p = 0 # sometimes rounding takes us to 2 or -1
+			train_pred_y.append(p)
+		train_error = percentError(train_pred_y, ds.train.y)
+
+		if i%100 == 0 or i==iterations-1:
+			epochs.append(i)
+			train_acc.append(100. - train_error)
+			print "Train error", train_error
+			# Compute percent test error
+			pred_y = []
+			for row in ds.test.x:
+				p = int( round( net.activate(row)[0] ) )
+				if p >= 1: p = 1 
+				else: p = 0 # sometimes rounding takes us to 2 or -1
+				pred_y.append(p)
+			print "Test error: " + str(percentError(pred_y, ds.test.y))
+			test_acc.append(100. - percentError(pred_y, ds.test.y))
+	
+	plot_errors(epochs, train_acc, test_acc)
+	generate_report(pred_y, ds.test.y)
+
+# print "NEURAL NETS"
+# test_neural_nets(heart)
+# nn_sphere = DataSet() # modify sphere to work with neural nets
+# nn_sphere.train.x = sphere.train.x
+# nn_sphere.train.y = [ 1 if a=='g' else 0 for a in sphere.train.y ]
+# nn_sphere.test.x = sphere.test.x
+# nn_sphere.test.y = [ 1 if a=='g' else 0 for a in sphere.test.y ]
+# test_neural_nets(nn_sphere)
+
+def test_boosting(ds, algorithm="SAMME"):
+	estimators = [10,20,30,50]
+	depths = [2,4,6,None]
+	print "depth | # estimators | precision | recall | f1-score"
+	for depth in depths:
+		for e in estimators:
+			bdt = AdaBoostClassifier(
+		    	tree.DecisionTreeClassifier(max_depth=2),
+			    n_estimators=e,
+			    algorithm=algorithm)
+			bdt.fit(ds.train.x, ds.train.y)
+			pred_y = [ bdt.predict(x)[0] for x in ds.test.x ]
+			print "Depth:", depth, "Number of estimators:", e
+			print bdt.score( ds.test.x, ds.test.y )
+			generate_report(pred_y, ds.test.y)
+		print "........."
+			
+
+# print "BOOSTING"
+# print "Heart SPECT"
+# test_boosting(heart, algorithm="SAMME")
+# print "Ionosphere"
+# test_boosting(sphere, algorithm="SAMME.R")
+
+
+def test_svm(ds):
+	kernels = ['linear', 'poly', 'rbf']
+	for k in kernels:
+		if k != 'linear':
+			for gamma in [0.1, 0.5, 1.]:
+				svc = svm.SVC(kernel=k, gamma=gamma)
+				svc.fit(ds.train.x, ds.train.y)
+				pred_y = [ svc.predict(x)[0] for x in ds.test.x ]
+				print "Kernel:", k, "Gamma:", gamma
+				print svc.score( ds.test.x, ds.test.y )
+				generate_report(pred_y, ds.test.y)
+		else:
+			svc = svm.SVC(kernel=k)
+			svc.fit(ds.train.x, ds.train.y)
+			pred_y = [ svc.predict(x)[0] for x in ds.test.x ]
+			print "Kernel:", k
+			print svc.score( ds.test.x, ds.test.y )
+			generate_report(pred_y, ds.test.y)
+
+
+print "SVM"
+print "Heart SPECT"
+test_svm(heart)
+print "Ionosphere"
+test_svm(sphere)
+print '*'*50
+
+
+
+
